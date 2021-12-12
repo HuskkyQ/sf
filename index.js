29bead724420013e820de1da542696f75b02ad4a
sevan <981740876@qq.com>
2021-12-13 00:23:02 +0800

    update 20211213

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
-module.exports = function sf () {
-    console.log('hello sf!')
+// npm publish 之前要更新版本号
+function sf () {
+    console.log('hello sff!')
+}
+
+function sf1 () {
+    console.log('hello sff1!')
+}
+
+const debounce = (fn, delay = 500) => {
+    let timer = null;
+    return function () {
+        const context = this, args = arguments;
+        timer && clearTimeout(timer);
+        timer = setTimeout(() => {
+            fn.apply(context, args);
+        }, delay)
+    }
+}
+
+const throttle = (fn, delay = 500) => {
+    let timer = null;
+    let startTime = Date.now();
+    return function () {
+        const context = this, args = arguments;
+        let remaining = delay - (Date.now() - startTime);
+        clearTimeout(timer);
+        if (remaining <= 0) {
+            fn.apply(context, args);
+            startTime = Date.now();
+        } else {
+            timer = setTimeout(fn, remaining);
+        }
+    }
+}
+
+module.exports = {
+    sf,
+    sf1,
+    debounce,
+    throttle
 }
---
+++ skill.md
@@ -0,0 +1,13 @@
+// 假设当前版本号 v1.0.0
+
+npm version patch
+// v1.0.1
+
+npm version prepatch
+// v1.0.2-0
+
+npm version minor
+// v1.1.0
+
+npm version major
+// v2.0.0
